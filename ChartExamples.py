01ad31bcd1885ce1706005114a1f6bbe1c6948c5
Alexandr Gončarov <69403882+Agoncarov7@users.noreply.github.com>
2023-05-05 23:32:03 +0200

    Add button close window

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,10 +2,18 @@
 
 import tkinter as tk
 
+# Функция закрытия программы
+def do_close():
+    window.destroy()
+
 # Создание главного окна 
 window = tk.Tk()
 window.geometry("550x550")
 window.title("Примеры построения графиков")
+
+# Добавление кнопки закрытия программы
+btnClose = tk.Button(window, text= "Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
+btnClose.place(x=400, y=480, width=90, height=30)
 
 # Запуск цикла mainloop
 window.mainloop()
