b4c094d8d3ef05bfe1b940555d15a3f7f021bf2b
Alexandr Gončarov <69403882+Agoncarov7@users.noreply.github.com>
2023-05-06 15:38:12 +0200

    First button sinus and external Chart1 file

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,6 +2,9 @@
 
 import tkinter as tk
 
+# Импорт внешних файлов
+import Chart1
+
 # Функция закрытия программы
 def do_close():
     window.destroy()
@@ -10,6 +13,17 @@ def do_close():
 window = tk.Tk()
 window.geometry("550x550")
 window.title("Примеры построения графиков")
+
+# Добавление метки заголовка
+lblTitle = tk.Label(text = "Примеры построения графиков", font = ('Tahoma', 14, 'bold'), fg = '#0000FF')
+lblTitle.place(x=65, y=45)
+
+# Добавление кнопки и метки заголовка 
+btnChart1 = tk.Button(window, text="График 1", font = ('Tahoma', 10, 'bold'),command=Chart1.plot_chart)
+btnChart1.place(x=40, y=115, width=100, height=30)
+
+lblChart1 = tk.Label(text = "График синуса MatPlotLib")
+lblChart1.place (x= 170, y=122)
 
 # Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text= "Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
