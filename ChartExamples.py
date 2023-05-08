30d25bafdf388b6ec54adaa3ad3831fc9998564c
Alexandr Gončarov <69403882+Agoncarov7@users.noreply.github.com>
2023-05-08 09:14:24 +0200

    Add 7 chart buttons

--- ChartExamples.py
+++ ChartExamples.py
@@ -22,29 +22,56 @@ lblTitle.place(x=65, y=45)
 
 # Добавление кнопки и метки для графика 1 
 btnChart1 = tk.Button(window, text="График 1", font = ('Tahoma', 10, 'bold'),command=Chart1.plot_chart)
-btnChart1.place(x=40, y=115, width=100, height=30)
+btnChart1.place(x=40, y=120, width=100, height=30)
 
 lblChart1 = tk.Label(text = "График синуса MatPlotLib")
-lblChart1.place (x= 170, y=120)
+lblChart1.place (x= 170, y=125)
 
 # Добавление кнопки и метки для графика 2 
 btnChart1 = tk.Button(window, text="График 2", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
-btnChart1.place(x=40, y=180, width=100, height=30)
+btnChart1.place(x=40, y=170, width=100, height=30)
 
 lblChart1 = tk.Label(text = "Нормальное распределение")
-lblChart1.place (x= 170, y=185)
+lblChart1.place (x= 170, y=175)
 
-# Добавление кнопки и метки для графика 3- нормальное распределение по уроку 
+# Добавление кнопки и метки для графика 3 
 btnChart1 = tk.Button(window, text="График 3", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
-btnChart1.place(x=40, y=245, width=100, height=30)
+btnChart1.place(x=40, y=220, width=100, height=30)
 
-lblChart1 = tk.Label(text = "Нормальное распределение урок")
-lblChart1.place (x= 170, y=250)
+lblChart1 = tk.Label(text = "Нормальное распределение 5.4")
+lblChart1.place (x= 170, y=225)
 
+# Добавление кнопки и метки для графика 4 
+btnChart1 = tk.Button(window, text="График 4", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
+btnChart1.place(x=40, y=270, width=100, height=30)
+
+lblChart1 = tk.Label(text = "Место для графика")
+lblChart1.place (x= 170, y=275)
+
+# Добавление кнопки и метки для графика 5 
+btnChart1 = tk.Button(window, text="График 5", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
+btnChart1.place(x=40, y=320, width=100, height=30)
+
+lblChart1 = tk.Label(text = "Место для графика")
+lblChart1.place (x= 170, y=325)
+
+# Добавление кнопки и метки для графика 6 
+btnChart1 = tk.Button(window, text="График 6", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
+btnChart1.place(x=40, y=370, width=100, height=30)
+
+lblChart1 = tk.Label(text = "Место для графика")
+lblChart1.place (x= 170, y=375)
+
+# Добавление кнопки и метки для графика 7 
+btnChart1 = tk.Button(window, text="График 7", font = ('Tahoma', 10, 'bold'),command=Chart2.plot_chart)
+btnChart1.place(x=40, y=420, width=100, height=30)
+
+lblChart1 = tk.Label(text = "Место для графика")
+lblChart1.place (x= 170, y=425)
 
 
 # Добавление кнопки закрытия программы
-btnClose = tk.Button(window, text= "Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
+btnClose = tk.Button(window, text= "Закрыть", font = ('Tahoma', 10, 'bold'), command=do_close)
 btnClose.place(x=400, y=480, width=90, height=30)
 
 # Запуск цикла mainloop
